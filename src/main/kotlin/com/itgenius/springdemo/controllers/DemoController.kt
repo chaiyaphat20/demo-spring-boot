e32d5a1d7a69554f15d00ea20113324f5b4da61d
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2024-07-16 21:01:29 +0700

    init

---
+++ src/main/kotlin/com/itgenius/springdemo/controllers/DemoController.kt
@@ -0,0 +1,28 @@
+package com.itgenius.springdemo.controllers
+
+import com.itgenius.springdemo.models.Demodata
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1")
+class DemoController {
+    @GetMapping("/hello")
+    fun hello():String {
+        return  "Hello,Spring boot"
+    }
+
+    @GetMapping("/about")
+    fun about():String {
+        return  "About,Spring boot"
+    }
+
+    @GetMapping("/demodata")
+    fun getDemoData():List<Demodata> {
+        return  listOf(
+            Demodata(1,"Art", "chaiyaphat@gmail.com","09988848484","123 bank"),
+            Demodata(1,"Art", "chaiyaphat@gmail.com","09988848484","123 bank"),
+        )
+    }
+}
