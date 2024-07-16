d16fd15af42aad7f231649f5a3179c5703a9878a
chaiyaphat <chaiyaphat.b5922314@gmail.com>
2024-07-16 21:08:38 +0700

    feat: add gey by id

--- src/main/kotlin/com/itgenius/springdemo/controllers/DemoController.kt
+++ src/main/kotlin/com/itgenius/springdemo/controllers/DemoController.kt
@@ -2,6 +2,7 @@ package com.itgenius.springdemo.controllers
 
 import com.itgenius.springdemo.models.Demodata
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,11 +19,18 @@ class DemoController {
         return  "About,Spring boot"
     }
 
+//    http://localhost:8080/api/v1/demodata
     @GetMapping("/demodata")
     fun getDemoData():List<Demodata> {
         return  listOf(
             Demodata(1,"Art", "chaiyaphat@gmail.com","09988848484","123 bank"),
             Demodata(2,"Jane", "chaiyaphat@gmail.com","09988848484","123 bank"),
         )
+    }
+
+//    http://localhost:8080/api/v1/demodata/2
+    @GetMapping("/demodata/{id}")
+    fun getDemoDataById(@PathVariable id:Int):Demodata {
+        return Demodata(id,"Art", "chaiyaphat@gmail.com","09988848484","123 bank")
     }
 }
